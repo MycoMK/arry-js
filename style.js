1028215579b7f7faf26338a5070752e08dd050c0
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-18 16:36:44 -0800

    arrys-js

---
+++ style.js
@@ -0,0 +1,19 @@
+alert('connected')
+
+
+let name = 'Fenway Park'
+const FullName = name.split('')
+console.log(FullName)
+
+let stream =' Google, FaceBook, Microsoft, Apple, IBM, Oracle, Amazon'
+const MainStream =stream.split(',')
+console.log(MainStream)
+
+const fruits = ['banana', 'oranges', 'mango', 'lemon']
+const vegetables = ['Tomatos', 'Potatos', 'Onion', 'Carrot']
+const fruitAndVegetables = fruits.concat(vegetables)
+
+console.log(fruitAndVegetables)
+
+
+
